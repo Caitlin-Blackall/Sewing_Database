ae88c1c950b6a8995772dae1b494e5a077aaba4e
Caitlin B <caitlin.blackall@gmail.com>
2023-07-03 13:49:27 +0200

    Completed search by category function

--- main.py
+++ main.py
@@ -213,22 +213,114 @@ def search():
     by_availability.grid(row=1, column=2, columnspan=2, sticky='E')
 
     go_back = tkinter.Button(frame, text='Go Back', command=go_back, height=1, width=7, pady=2)
-    go_back.grid(row=8, column=3, columnspan=1, sticky='E')
+    go_back.grid(row=8, column=0, columnspan=1, sticky='W')
 
     fourth.mainloop()
 
     if search_option == 'Cat':
-        print('cat')
+        by_cat()
     elif search_option == 'Avail':
         print('avail')
     elif search_option == 'Go Back':
         run()
     else:
         return None
-    '''parameter = input('Do you want to search by pattern category (A) or printed patterns (B)? ').upper()
-    if parameter == 'A':
-        item = 'Yay'
-        print('The pattern/s that match the search are {}'.format(item))'''
+
+def by_cat():
+    def go_back():
+        five.destroy()
+        search()
+
+    def cats_tosearchby():
+        categories_tosearch = []
+        add_bathers = CheckVar1.get()
+        add_bottoms = CheckVar2.get()
+        add_dresses = CheckVar3.get()
+        add_jumpers = CheckVar4.get()
+        add_male = CheckVar5.get()
+        add_misc = CheckVar6.get()
+        add_rompers = CheckVar7.get()
+        add_tops = CheckVar8.get()
+
+        if add_bathers == 1:
+            categories_tosearch.append('Bathers')
+        if add_bottoms == 1:
+            categories_tosearch.append('Bottoms')
+        if add_dresses == 1:
+            categories_tosearch.append('Dresses')
+        if add_jumpers == 1:
+            categories_tosearch.append('Jumpers')
+        if add_male == 1:
+            categories_tosearch.append('Male')
+        if add_misc == 1:
+            categories_tosearch.append('Misc')
+        if add_rompers == 1:
+            categories_tosearch.append('Rompers')
+        if add_tops == 1:
+            categories_tosearch.append('Tops')
+        with open('data.csv', 'r') as csv_file:
+            spreadsheet = csv.DictReader(csv_file)
+            pattern_names = []
+            for row in spreadsheet:
+                for category in categories_tosearch:
+                    if category in row['Category']:
+                        pattern_names.append(row['Pattern Name'])
+        pattern_names = list(dict.fromkeys(pattern_names))
+        finalnames_bycat = ''
+        x = len(pattern_names)
+        for name in pattern_names:
+            index = pattern_names.index(name)
+            if index == 0:
+                finalnames_bycat += name
+            else:
+                finalnames_bycat += '\n' + name
+        if x == 0:
+            print('There are no pattern matches.')
+        elif x == 1:
+            print('There is 1 pattern match.\nThe pattern is: {}.'.format(finalnames_bycat))
+        else:
+            print('There are {} pattern matches.\nThe patterns are:\n{}'.format(x, finalnames_bycat))
+        quit()
+
+    five = tkinter.Tk()
+    five.title('Search by Category')
+    frame = ttk.Frame(five, padding=5)
+    frame.grid()
+
+    label_1 = tkinter.Label(frame, text='Select category / categories to search by:', pady=10)
+    label_1.grid(row=0, column=0, columnspan=3, sticky='W')
+    CheckVar1 = tkinter.IntVar()
+    CheckVar2 = tkinter.IntVar()
+    CheckVar3 = tkinter.IntVar()
+    CheckVar4 = tkinter.IntVar()
+    CheckVar5 = tkinter.IntVar()
+    CheckVar6 = tkinter.IntVar()
+    CheckVar7 = tkinter.IntVar()
+    CheckVar8 = tkinter.IntVar()
+    C1 = tkinter.Checkbutton(frame, text='Bathers', variable=CheckVar1, onvalue=1, offvalue=0, width=10)
+    C2 = tkinter.Checkbutton(frame, text='Bottoms', variable=CheckVar2, onvalue=1, offvalue=0, width=10)
+    C3 = tkinter.Checkbutton(frame, text='Dresses', variable=CheckVar3, onvalue=1, offvalue=0, width=10)
+    C4 = tkinter.Checkbutton(frame, text='Jumpers', variable=CheckVar4, onvalue=1, offvalue=0, width=10)
+    C5 = tkinter.Checkbutton(frame, text='Male', variable=CheckVar5, onvalue=1, offvalue=0, width=7)
+    C6 = tkinter.Checkbutton(frame, text='Misc', variable=CheckVar6, onvalue=1, offvalue=0, width=7)
+    C7 = tkinter.Checkbutton(frame, text='Rompers', variable=CheckVar7, onvalue=1, offvalue=0, width=10)
+    C8 = tkinter.Checkbutton(frame, text='Tops', variable=CheckVar8, onvalue=1, offvalue=0, width=7)
+    C1.grid(row=1, column=0, sticky='W')
+    C2.grid(row=2, column=0, sticky='W')
+    C3.grid(row=1, column=1, sticky='W')
+    C4.grid(row=2, column=1, sticky='W')
+    C5.grid(row=1, column=2, sticky='W')
+    C6.grid(row=2, column=2, sticky='W')
+    C7.grid(row=1, column=3, sticky='W')
+    C8.grid(row=2, column=3, sticky='W')
+
+    go_back = tkinter.Button(frame, text='Go Back', command=go_back, height=1, width=7, pady=2)
+    go_back.grid(row=3, column=0, columnspan=1, sticky='SW')
+
+    start_search = tkinter.Button(frame, text='Start Search', command=cats_tosearchby, height=2, width=14, pady=2)
+    start_search.grid(row=3, column=3, columnspan=1, sticky='E')
+
+    five.mainloop()
 
 def update():
     print('Done')
